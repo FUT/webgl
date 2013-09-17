62d7011ed545bdbf877e3ec704d41beca5c44ac2
Yury Kaliada <fut.wrk@gmail.com>
2013-09-17 22:14:14 +0300

    initial commit
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webgl::Application.config.secret_token = 'fa09dab6db25ca81f316fc57bc1be2317d8c7a53804f9b8256c4380ff1d8097d38de316d5bf769a436c1c775fd967c818489c169bf7a5b77d876d83bb9df06e1'
